ff7c7fe87027af7fd78449949f6f0f54b2e7d737
ZHOUJUNX <40539479+ZHOUJUNX@users.noreply.github.com>
2023-05-02 14:50:40 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,31 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# Modify default IP Gate #
+sed -i 's/192.168.1.1/10.1.1.1/g' package/base-files/files/bin/config_generate
+echo '### Updates default IP gate ###'
+
+# Updates Theme Argon #
+rm -rf package/feeds/luci/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/feeds/luci/luci-theme-argon
+echo '### Updates Theme Argon ###'
+
+# Argon Theme Config plug-in components #
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
+echo '### Argon Theme Config ###'
+
+# luci-app-adguardhome plug-in components #
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/lean/luci-app-adguardhome
+echo '### luci-app-adguardhome ###'
+
+# luci-app-smartdns plug-in components #
+git clone -b lede https://github.com/pymumu/luci-app-smartdns package/lean/luci-app-smartdns
+echo '### luci-app-smartdns ###'
+
+# OpenAppFilter plug-in components #
+# git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+# echo '### OpenAppFilter ###'
+
+# Shutdown plug-in components #
+# git clone https://github.com/esirplayground/luci-app-poweroff.git package/lean/luci-app-poweroff
+# echo '### Shutdown Router ###'
